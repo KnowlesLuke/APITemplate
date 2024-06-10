fba8e7a40286bfa6246db79e54b6a26393dececd
KnowlesLuke <luke.knowles@tameside.gov.uk>
2024-06-10 11:59:30 +0100

    Added single account response

--- UnitTests/AccountsReadService_GetAccounts.cs
+++ UnitTests/AccountsReadService_GetAccounts.cs
@@ -14,29 +14,35 @@ namespace UnitTests
         // Mock IAccountsReadService
         private readonly Mock<IAccountsReadService> _accountsReadService;
 
-        // Account Response
+        // Accounts Response
         private readonly IEnumerable<AccountResponse> _accountsResponse;
+
+        // Account Response
+        private readonly AccountResponse _accountResponse;
 
         public AccountsReadService_GetAccounts()
         {
             // Setup Mock AccountsReadService
             _accountsReadService = new Mock<IAccountsReadService>();
 
-            // Setup Account Response - Arrange
+            // Setup account response
+            _accountResponse = new AccountResponse
+            (
+                Id: 1,
+                Forename: "Test Forename",
+                Surname: "Surname",
+                DisplayName: "Test Forename Surname",
+                Username: "Username",
+                Email: "Email Address",
+                RoleId: 1,
+                Created: DateTime.Now,
+                Modified: DateTime.Now
+            );
+
+            // Setup Account Response - Arrange and Add account response to the list
             _accountsResponse = new List<AccountResponse>
             {
-                new
-                (
-                    Id: 1,
-                    Forename: "Test Forename",
-                    Surname: "Surname",
-                    DisplayName: "Test Forename Surname",
-                    Username: "Username",
-                    Email: "Email Address",
-                    RoleId: 1,
-                    Created: DateTime.Now,
-                    Modified: DateTime.Now
-                )
+                _accountResponse
             };
         }
 
@@ -52,7 +58,10 @@ namespace UnitTests
             // Assert
             Assert.NotNull(result);
 
+            // Check if the result is equal to the accountResponse
             Assert.Equal(_accountsResponse, result);
+
+            // Check if the result is of type IEnumerable<AccountResponse>
             Assert.IsAssignableFrom<IEnumerable<AccountResponse>>(result);
         }
 
@@ -60,7 +69,7 @@ namespace UnitTests
         public async Task GetAccountByIdAsync_ValidAccountId_ReturnsAccountResponse()
         {
             // Setup GetAccountByIdAsync - If accountId is passed, return corresponding accountResponse
-            _accountsReadService.Setup(x => x.GetAccountByIdAsync(1)).ReturnsAsync(_accountsResponse.FirstOrDefault(x => x.Id == 1));
+            _accountsReadService.Setup(x => x.GetAccountByIdAsync(1)).ReturnsAsync(_accountResponse);
 
             // Act
             var result = await _accountsReadService.Object.GetAccountByIdAsync(1);
@@ -68,7 +77,7 @@ namespace UnitTests
             // Assert
             Assert.NotNull(result);
 
-            Assert.Equal(_accountsResponse.FirstOrDefault(), result);
+            Assert.Equal(_accountResponse, result);
             Assert.IsAssignableFrom<AccountResponse>(result);
         }
 
